a51e0670bfbf26b9de9882fb9f88240078ea38b9
Dat <dathoang31082001@gmail.com>
2022-05-19 20:11:27 +0700

    connect to DB with sequelize

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,17 @@
-let getController = (req, res) => {
-    return res.render('homePage.ejs');
+import { json } from "express/lib/response";
+import db from "../models/index";
+
+let getController = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('homePage.ejs', {
+            data: JSON.stringify(data),
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+
 }
 module.exports = {
     getController: getController,
