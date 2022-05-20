b0bc2b0f94431517981c8abfa96cbd0d0c3237fc
Dat <dathoang31082001@gmail.com>
2022-05-20 21:08:17 +0700

    update user, allcode DB

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('firsttest', 'root', null, {
+const sequelize = new Sequelize('bookingcare', 'root', null, {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
