f27ca237013c34d4709b1316749e3449d0cf2482
Dat <dathoang31082001@gmail.com>
2022-06-07 22:16:05 +0700

    validate & hash pasword

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,23 @@
-let handleLogin = (req, res) => {
+import userService from '../services/userService';
+
+
+let handleLogin = async (req, res) => {
+    let password = req.body.password;
+    let email = req.body.email;
+
+    if (!email || !password) {
+        return res.status(500).json({
+            errCode: 1,
+            message: 'Missing parameter!!',
+        })
+    }
+
+    let userData = await userService.handleUserLogin(email, password);
+
     return res.status(200).json({
-        message: "Hello world"
+        errCode: userData.errCode,
+        message: userData.errMessage,
+        userData,
     })
 }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,15 +1,48 @@
 import db from '../models/index';
+const bcrypt = require('bcrypt');
 
 let handleUserLogin = (email, password) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let isExist = checkUserEmail(email);
+            let userData = {};
+            let isExist = await checkUserEmail(email);
             if (isExist) {
-
+                let user = await db.User.findOne({
+                    where: { email: email },
+                });
+                if (user) {
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'OK';
+                        userData.user = user;
+                    }
+                    else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'WRONG PASSWORD!!';
+                        userData.user = user;
+                    }
+                }
+                else {
+                    userData.errCode = 2;
+                    userData.message = 'User not found';
+                }
             }
             else {
-
+                userData.errCode = 1;
+                userData.errMessage = `Your Email isn't exist. Plz try other email!`;
             }
+            resolve(userData);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let compareUserPassword = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
         } catch (e) {
             reject(e);
         }
@@ -37,5 +70,4 @@ let checkUserEmail = (userEmail) => {
 
 module.exports = {
     handleUserLogin: handleUserLogin,
-    checkUserEmail: checkUserEmail,
 }
